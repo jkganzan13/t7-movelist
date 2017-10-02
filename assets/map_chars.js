a87e6bea15234958eaa036ac74bb051978a62352
John Kenneth Ganzan <jkganzan13@gmail.com>
2017-10-02 19:24:09 +1300

    * completed Paul * mapped all chars * when test passes for char, copy to /done

--- assets/map_chars.js
+++ assets/map_chars.js
@@ -4,7 +4,8 @@ module.exports = [
     'n':'Akuma',
     'i': 32,
     'moveList': require('./movelists/MOVELIST_32'),
-    'rbn': require('./rbnorway/t7-akuma-1.json')
+    'rbn': require('./rbnorway/t7-akuma-1.json'),
+    'length': 72
   },
   {
     'c': 'ALISA',
@@ -12,203 +13,231 @@ module.exports = [
     'i': 19,
     'moveList': require('./movelists/MOVELIST_19'),
     'rbn': require('./rbnorway/t7-alisa-1.json'),
-    'length': 115,
+    'length': 115
   },
   {
     'c': 'ASUKA',
     'n':'Asuka Kazama',
     'i': 12,
     'moveList': require('./movelists/MOVELIST_12'),
-    'rbn': require('./rbnorway/t7-asuka-1.json')
+    'rbn': require('./rbnorway/t7-asuka-1.json'),
+    'length': 75
   },
   {
     'c': 'BOB',
     'n':'Bob Richards',
     'i': 31,
     'moveList': require('./movelists/MOVELIST_31'),
-    'rbn': require('./rbnorway/t7-bob-1.json')
+    'rbn': require('./rbnorway/t7-bob-1.json'),
+    'length': 85
   },
   {
     'c': 'BRYAN',
     'n':'Bryan Fury',
     'i': 7,
     'moveList': require('./movelists/MOVELIST_07'),
-    'rbn': require('./rbnorway/t7-bryan-1.json')
+    'rbn': require('./rbnorway/t7-bryan-1.json'),
+    'length': 79
   },
   {
     'c': 'LUCKY CHLOE',
     'n':'Lucky Chloe',
     'i': 22,
     'moveList': require('./movelists/MOVELIST_22'),
-    'rbn': require('./rbnorway/t7-lucky-chloe-1.json')
+    'rbn': require('./rbnorway/t7-lucky-chloe-1.json'),
+    'length': 75
   },
   {
     'c': 'CLAUDIO',
     'n':'Claudio Serafino',
     'i': 20,
     'moveList': require('./movelists/MOVELIST_20'),
-    'rbn': require('./rbnorway/t7-claudio-1.json')
+    'rbn': require('./rbnorway/t7-claudio-1.json'),
+    'length': 48
   },
   {
     'c': 'DEVIL JIN',
     'n':'Devil Jin',
     'i': 13,
     'moveList': require('./movelists/MOVELIST_13'),
-    'rbn': require('./rbnorway/t7-devil-jin-1.json')
+    'rbn': require('./rbnorway/t7-devil-jin-1.json'),
+    'length': 72
   },
   {
     'c': 'DRAGUNOV',
     'n':'Sergei Dragunov',
     'i': 16,
     'moveList': require('./movelists/MOVELIST_16'),
-    'rbn': require('./rbnorway/t7-dragunov-1.json')
+    'rbn': require('./rbnorway/t7-dragunov-1.json'),
+    'length': 72
   },
   {
     'c': 'EDDY',
     'n':'Eddy Gordo',
     'i': 35,
     'moveList': require('./movelists/MOVELIST_35'),
-    'rbn': require('./rbnorway/t7-eddy-1.json')
+    'rbn': require('./rbnorway/t7-eddy-1.json'),
+    'length': 151
   },
   {
     'c': 'ELIZA',
     'n':'Eliza',
     'i': 36,
     'moveList': require('./movelists/MOVELIST_36'),
-    'rbn': require('./rbnorway/t7-eliza-1.json')
+    'rbn': require('./rbnorway/t7-eliza-1.json'),
+    'length': 69
   },
   {
     'c': 'FENG',
     'n':'Feng Wei',
     'i': 14,
     'moveList': require('./movelists/MOVELIST_14'),
-    'rbn': require('./rbnorway/t7-feng-1.json')
+    'rbn': require('./rbnorway/t7-feng-1.json'),
+    'length': 83
   },
   {
     'c': 'GIGAS',
     'n':'GIGAS',
     'i': 25,
     'moveList': require('./movelists/MOVELIST_25'),
-    'rbn': require('./rbnorway/t7-gigas-1.json')
+    'rbn': require('./rbnorway/t7-gigas-1.json'),
+    'length': 64
   },
   {
     'c': 'HEIHACHI',
     'n':'Heihachi Mishima',
     'i': 8,
     'moveList': require('./movelists/MOVELIST_08'),
-    'rbn': require('./rbnorway/t7-heihachi-1.json')
+    'rbn': require('./rbnorway/t7-heihachi-1.json'),
+    'length': 64
   },
   {
     'c': 'HWOARANG',
     'n':'Hwoarang',
     'i': 4,
     'moveList': require('./movelists/MOVELIST_04'),
-    'rbn': require('./rbnorway/t7-hwoarang-1.json')
+    'rbn': require('./rbnorway/t7-hwoarang-1.json'),
+    'length':129
   },
   {
     'c': 'JACK-7',
     'n':'JACK-7',
     'i': 11,
     'moveList': require('./movelists/MOVELIST_11'),
-    'rbn': require('./rbnorway/t7-jack-7-1.json')
+    'rbn': require('./rbnorway/t7-jack-7-1.json'),
+    'length':78
   },
   {
     'c': 'JIN',
     'n':'Jin Kazama',
     'i': 6,
     'moveList': require('./movelists/MOVELIST_06'),
-    'rbn': require('./rbnorway/t7-jin-1.json')
+    'rbn': require('./rbnorway/t7-jin-1.json'),
+    'length':64
   },
   {
     'c': 'JOSIE',
     'n':'Josie Rizal',
     'i': 24,
     'moveList': require('./movelists/MOVELIST_24'),
-    'rbn': require('./rbnorway/t7-josie-1.json')
+    'rbn': require('./rbnorway/t7-josie-1.json'),
+    'length':69
   },
   {
     'c': 'KATARINA',
     'n':'Katarina Alves',
     'i': 21,
     'moveList': require('./movelists/MOVELIST_21'),
-    'rbn': require('./rbnorway/t7-katarina-1.json')
+    'rbn': require('./rbnorway/t7-katarina-1.json'),
+    'length':59
   },
   {
     'c': 'KAZUMI',
     'n':'Kazumi Mishima',
     'i': 26,
     'moveList': require('./movelists/MOVELIST_26'),
-    'rbn': require('./rbnorway/t7-kazumi-1.json')
+    'rbn': require('./rbnorway/t7-kazumi-1.json'),
+    'length':48
   },
   {
     'c': 'KAZUYA',
     'n':'Kazuya Mishima',
     'i': 9,
     'moveList': require('./movelists/MOVELIST_09'),
-    'rbn': require('./rbnorway/t7-kazuya-1.json')
+    'rbn': require('./rbnorway/t7-kazuya-1.json'),
+    'length':65
   },
   {
     'c': 'KING',
     'n':'King',
     'i': 2,
     'moveList': require('./movelists/MOVELIST_02'),
-    'rbn': require('./rbnorway/t7-king-1.json')
+    'rbn': require('./rbnorway/t7-king-1.json'),
+    'length':83
   },
   {
     'c': 'KUMA',
     'n':'Kuma',
     'i': 33,
     'moveList': require('./movelists/MOVELIST_33'),
-    'rbn': require('./rbnorway/t7-kuma-1.json')
+    'rbn': require('./rbnorway/t7-kuma-1.json'),
+    'length':89
   },
   {
     'c': 'LARS',
     'n':'Lars Alexandersson',
     'i': 18,
     'moveList': require('./movelists/MOVELIST_18'),
-    'rbn': require('./rbnorway/t7-lars-1.json')
+    'rbn': require('./rbnorway/t7-lars-1.json'),
+    'length':73
   },
   {
     'c': 'LAW',
     'n':'Marshall Law',
     'i': 1,
     'moveList': require('./movelists/MOVELIST_01'),
-    'rbn': require('./rbnorway/t7-law-1.json')
+    'rbn': require('./rbnorway/t7-law-1.json'),
+    'length':98
   },
   {
     'c': 'LEE',
     'n':'Lee Chaolan',
     'i': 30,
     'moveList': require('./movelists/MOVELIST_30'),
-    'rbn': require('./rbnorway/t7-lee-1.json')
+    'rbn': require('./rbnorway/t7-lee-1.json'),
+    'length':91
   },
   {
     'c': 'LEO',
     'n':'Leo Kliesen',
     'i': 17,
     'moveList': require('./movelists/MOVELIST_17'),
-    'rbn': require('./rbnorway/t7-leo-1.json')
+    'rbn': require('./rbnorway/t7-leo-1.json'),
+    'length':72
   },
   {
     'c': 'LILI',
     'n':'Lili Rochefort',
     'i': 15,
     'moveList': require('./movelists/MOVELIST_15'),
-    'rbn': require('./rbnorway/t7-lili-1.json')
+    'rbn': require('./rbnorway/t7-lili-1.json'),
+    'length':74
   },
   {
     'c': 'MIGUEL',
     'n':'Miguel Caballero Rojo',
     'i': 37,
     'moveList': require('./movelists/MOVELIST_37'),
-    'rbn': require('./rbnorway/t7-miguel-1.json')
+    'rbn': require('./rbnorway/t7-miguel-1.json'),
+    'length':107
   },
   {
     'c': 'NINA',
     'n':'Nina Williams',
     'i': 28,
     'moveList': require('./movelists/MOVELIST_28'),
-    'rbn': require('./rbnorway/t7-nina-1.json')
+    'rbn': require('./rbnorway/t7-nina-1.json'),
+    'length':98
   },
   // {
 	//   'c': 'PANDA',
@@ -222,41 +251,47 @@ module.exports = [
     'n':'Paul Phoenix',
     'i': 0,
     'moveList': require('./movelists/MOVELIST_00'),
-    'rbn': require('./rbnorway/t7-paul-1.json')
+    'rbn': require('./rbnorway/t7-paul-1.json'),
+    'length': 57
   },
   {
     'c': 'RAVEN',
     'n':'Master Raven',
     'i': 29,
     'moveList': require('./movelists/MOVELIST_29'),
-    'rbn': require('./rbnorway/t7-master-raven-1.json')
+    'rbn': require('./rbnorway/t7-master-raven-1.json'),
+    'length':111
   },
   {
     'c': 'SHAHEEN',
     'n':'Shaheen',
     'i': 23,
     'moveList': require('./movelists/MOVELIST_23'),
-    'rbn': require('./rbnorway/t7-shaheen-1.json')
+    'rbn': require('./rbnorway/t7-shaheen-1.json'),
+    'length':50
   },
   {
     'c': 'STEVE',
     'n':'Steve Fox',
     'i': 10,
     'moveList': require('./movelists/MOVELIST_10'),
-    'rbn': require('./rbnorway/t7-steve-1.json')
+    'rbn': require('./rbnorway/t7-steve-1.json'),
+    'length':114
   },
   {
     'c': 'XIAOYU',
     'n':'Ling Xiaoyu',
     'i': 5,
     'moveList': require('./movelists/MOVELIST_05'),
-    'rbn': require('./rbnorway/t7-ling-1.json')
+    'rbn': require('./rbnorway/t7-ling-1.json'),
+    'length':111
   },
   {
     'c': 'YOSHIMITSU',
     'n':'Yoshimitsu',
     'i': 3,
     'moveList': require('./movelists/MOVELIST_03'),
-    'rbn': require('./rbnorway/t7-yoshimitsu-1.json')
+    'rbn': require('./rbnorway/t7-yoshimitsu-1.json'),
+    'length':149
   }
 ]
--- src/t7.test.js
+++ src/t7.test.js
@@ -1,4 +1,5 @@
 const R = require('ramda');
+const fse = require('fs-extra');
 const map_chars = require('../assets/map_chars');
 
 const hasLength = c => !!c.length;
@@ -9,11 +10,12 @@ charsWithLength.forEach((char) => {
 	let notFound = [];
 	for(let i = 1; i < char['length']; i++) {
 		const move = R.find(R.propEq('number', i))(output.moves);
-		if(!move) notFound.push(true);
+		if(!move) notFound.push(i);
 	}
 	if(!!notFound.length) {
-		console.log(`${char.c} failed!`)
+		console.log(`${char.c} failed! Number ${notFound} missing!`)
 	} else {
 		console.log(`${char.c} passed!`)
+    fse.copySync(`t7/${char.c}.json`, `done/${char.c}.json`)
 	}
 })
